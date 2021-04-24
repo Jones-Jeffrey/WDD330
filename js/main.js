c93771782fb39a2b2300c9c16914e34603a71b91
Jones-Jeffrey <jeffgjones@gmx.com>
2021-04-23 23:55:14 -0500

    Week 1 Changes

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+window.onload = function () {
+	let list = document.getElementById('linkList');
+
+    const links = [
+        {
+        label: 'Week 1 Code',
+		url: '/WDD330/week1/'
+        }
+    ]
+        for (var i = links.length - 1; i >= 0; i--) {
+            let li = document.createElement('li');
+            let a = document.createElement('a');
+            a.textContent = links[i].label;
+            a.setAttribute('href', links[i].url);
+            li.append(a);
+            list.append(li);
+            
+        }
+}
+
