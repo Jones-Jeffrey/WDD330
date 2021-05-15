cd184c6dc26a05eb9b239da6679c59675f002a55
Jones-Jeffrey <jeffgjones@gmx.com>
2021-05-15 15:25:42 -0500

    week 4

--- js/main.js
+++ js/main.js
@@ -4,18 +4,23 @@ window.onload = function () {
 
     const links = [
         {
+            label: 'Week 4',
+            url:'week4/'
+
+        },
+        {
             label: 'Week 3',
-            url:'/week3/'
+            url:'week3/'
 
         },
         {
             label: 'Week 2',
-            url:'/week2/'
+            url:'week2/'
 
         },
         {
             label: 'Week 1',
-		    url: '/week1/'
+		    url: 'week1/'
         }
         
     ];
